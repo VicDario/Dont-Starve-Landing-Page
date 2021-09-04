fd1d0329d67eea1540c36e413bf3cd337cbefdd5
VicDario <26585356+VicDario@users.noreply.github.com>
2021-09-03 22:41:47 -0400

    Carousel ready(Only mobile)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,42 @@
-window.onload = () => {
-    let menuButton = document.getElementById('menu-button');
-    menuButton.addEventListener('click', () => {
-        let menu = document.getElementById('menu-list');
-        if(menu.classList.contains('hidden')) {
-            menu.classList.remove('hidden');
-            menu.classList.add('visible');
-        } else {
-            menu.classList.remove('visible');
-            menu.classList.add('hidden');
-        }
-    });
-}
+let menuButton = document.getElementById('menu-button');
+menuButton.addEventListener('click', () => {
+    let menu = document.getElementById('menu-list');
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+        menu.classList.add('visible');
+    } else {
+        menu.classList.remove('visible');
+         menu.classList.add('hidden');
+    }
+});
+
+let carouselIndex = 0;
+const slideImage = () => {
+    let elements = document.querySelectorAll('.carousel-element');
+    if (carouselIndex >= elements.length) {
+        carouselIndex = 0;
+    }
+    if (carouselIndex < 0) {
+        carouselIndex = elements.length -1;
+    }
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'none';
+    }
+    elements[carouselIndex].style.display = 'block';
+};
+let arrowPrevious = document.getElementById('arrow-previous');
+arrowPrevious.addEventListener('click', () => {
+    carouselIndex--;
+    slideImage();
+});
+
+let arrowNext = document.getElementById('arrow-next');
+arrowNext.addEventListener('click', () => {
+    carouselIndex++;
+    slideImage();
+});
+
+const slideInterval = setInterval(() => {
+    carouselIndex++;
+    slideImage();
+}, 5000);
