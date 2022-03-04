a6ec33fe54d017f0270e91c0c317fe25a27d657a
Víctor Fernández <26585356+VicDario@users.noreply.github.com>
2022-03-04 16:56:21 -0300

    [Fix] Import file deleted

--- js/index.js
+++ js/index.js
@@ -13,30 +13,36 @@ menuButton.addEventListener('click', () => {
 let carouselIndex = 0;
 const slideImage = () => {
     let elements = document.querySelectorAll('.carousel-element');
-    if (carouselIndex >= elements.length) {
-        carouselIndex = 0;
-    }
-    if (carouselIndex < 0) {
-        carouselIndex = elements.length -1;
-    }
+    if (carouselIndex >= elements.length)   carouselIndex = 0;
+    if (carouselIndex < 0)  carouselIndex = elements.length - 1;
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.display = 'none';
     }
     elements[carouselIndex].style.display = 'block';
 };
+
+const createInterval = () => {
+    return setInterval(() => {
+            carouselIndex++;
+            slideImage();
+        }, 5000);
+};
+
+let sliderInterval = createInterval()
+
 let arrowPrevious = document.getElementById('arrow-previous');
 arrowPrevious.addEventListener('click', () => {
     carouselIndex--;
     slideImage();
+    clearInterval(slideInterval);
+    sliderInterval = createInterval();
 });
 
 let arrowNext = document.getElementById('arrow-next');
 arrowNext.addEventListener('click', () => {
     carouselIndex++;
     slideImage();
+    clearInterval(slideInterval);
+    sliderInterval = createInterval();
 });
 
-const slideInterval = setInterval(() => {
-    carouselIndex++;
-    slideImage();
-}, 5000);
